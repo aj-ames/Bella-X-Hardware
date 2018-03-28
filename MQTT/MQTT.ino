1a21335628dbd4623db539d5ae70a8860de43047
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2018-03-28 13:13:50 +0530

    Bella-X: MQTT: Implement req logic & add debug code

--- MQTT/MQTT.ino
+++ MQTT/MQTT.ino
@@ -8,14 +8,11 @@ const char* password = "qazwsxedc@12345";
 const char* mqtt_username = "astr1x";
 const char* mqtt_password = "astr1x2096";
 
-// LED on ESP8266 GPIO5
-const int lightPin = 5;
-
 // Topic to subscribe to for the commands
-char* lightTopic = "bellax/req";
+char* subTopic = "bellax/req";
 
 // Topic to publish to confirm that the light has been turned on for the python script to log
-char* lightConfirmTopic = "bellax/ack";
+char* pubTopic = "bellax/ack";
 
 // Callback function header
 void callback(char* topic, byte* payload, unsigned int length);
@@ -24,124 +21,117 @@ WiFiClient wifiClient;
 PubSubClient client(MQTT_SERVER, 1883, callback, wifiClient);
 
 void setup() {
-//initialize the light as an output and set to LOW (off)
-pinMode(lightPin, OUTPUT);
-digitalWrite(lightPin, LOW);
+  // Start the serial line for debugging
+  Serial.begin(115200);
+  delay(100);
 
-//start the serial line for debugging
-Serial.begin(115200);
-delay(100);
+  // Start wifi subsystem
+  WiFi.begin(ssid, password);
 
+  // Attempt to connect to the WIFI network and then connect to the MQTT server
+  reconnect();
 
-//start wifi subsystem
-WiFi.begin(ssid, password);
-
-//attempt to connect to the WIFI network and then connect to the MQTT server
-reconnect();
-
-//wait a bit before starting the main loop
+  // Wait a bit before starting the main loop
   delay(2000);
 }
 
-void loop(){
+void loop() {
+  // Reconnect if connection is lost
+  if (!client.connected() && WiFi.status() == 3)
+    reconnect();
 
-//reconnect if connection is lost
-if (!client.connected() && WiFi.status() == 3)
-{reconnect();}
+  // Maintain MQTT connection
+  client.loop();
 
-//maintain MQTT connection
-client.loop();
-
-//MUST delay to allow ESP8266 WIFI functions to run
-delay(10); 
+  // MUST delay to allow ESP8266 WIFI functions to run
+  delay(10); 
 }
 
-//MQTT callback
+// MQTT callback function
 void callback(char* topic, byte* payload, unsigned int length) {
+  
+  // Convert topic to string to make it easier to work with
+  String topicStr = topic; 
 
-//convert topic to string to make it easier to work with
-String topicStr = topic; 
+  // Print out some debugging info
+  //Serial.println("Callback update.");
+  //Serial.print("Topic: ");
+  //Serial.print(topicStr);
+  for (int c = 0; c < length; c++)
+    Serial.write(payload[c]);
+  Serial.println();
+  //turn the light on if the payload is '1' and publish to the confirmation topic so the python script can log it
+  if(payload[0] == '1') {
+    //digitalWrite(lightPin, HIGH);
+    client.publish(pubTopic, "On");
+  }
 
-//Print out some debugging info
-Serial.println("Callback update.");
-Serial.print("Topic: ");
-Serial.print(topicStr);
-String message = String(*payload);
-Serial.print(message);
-//turn the light on if the payload is '1' and publish to the confirmation topic so the python script can log it
-if(payload[0] == '1'){
-  digitalWrite(lightPin, HIGH);
-  client.publish(lightConfirmTopic, "On");
-}
-
-//turn the light off if the payload is '0' and publish to the confirmation topic so the python script can log it
-else if (payload[0] == '0'){
-  digitalWrite(lightPin, LOW);
-  client.publish(lightConfirmTopic, "Off");
-}
+  //turn the light off if the payload is '0' and publish to the confirmation topic so the python script can log it
+  else if (payload[0] == '0') {
+    //digitalWrite(lightPin, LOW);
+    client.publish(pubTopic, "Off");
+  }
 }
 
 //networking functions
 
 void reconnect() {
 
-//attempt to connect to the wifi if connection is lost
-if(WiFi.status() != WL_CONNECTED){
-  //debug printing
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+  //attempt to connect to the wifi if connection is lost
+  if(WiFi.status() != WL_CONNECTED) {
+    //debug printing
+    // Serial.print("Connecting to ");
+    // Serial.println(ssid);
 
-  //loop while we wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    //loop while we wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      // Serial.print(".");
   }
 
   //print out some more debug once connected
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-}
-
-//make sure we are connected to WIFI before attemping to reconnect to MQTT
-if(WiFi.status() == WL_CONNECTED){
-// Loop until we're reconnected to the MQTT server
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-
-    // Generate client name based on MAC address and last 8 bits of microsecond counter
-    String clientName;
-    clientName += "esp8266-";
-    uint8_t mac[6];
-    WiFi.macAddress(mac);
-    clientName += macToStr(mac);
-
-    //if connected, subscribe to the topic(s) we want to be notified about
-    if (client.connect((char*) clientName.c_str(), mqtt_username, mqtt_password)) {
-      Serial.print("\tMTQQ Connected");
-      client.subscribe(lightTopic);
-    }
-
-    //otherwise print failed for debugging
-    else{Serial.println("\tFailed."); abort();}
+  // Serial.println("");
+  // Serial.println("WiFi connected");  
+  // Serial.println("IP address: ");
+  // Serial.println(WiFi.localIP());
   }
-}
+
+  //make sure we are connected to WIFI before attemping to reconnect to MQTT
+  if(WiFi.status() == WL_CONNECTED){
+  // Loop until we're reconnected to the MQTT server
+    while (!client.connected()) {
+      //Serial.print("Attempting MQTT connection...");
+
+      // Generate client name based on MAC address and last 8 bits of microsecond counter
+      String clientName;
+      clientName += "esp8266-";
+      uint8_t mac[6];
+      WiFi.macAddress(mac);
+      clientName += macToStr(mac);
+
+      //if connected, subscribe to the topic(s) we want to be notified about
+      if (client.connect((char*) clientName.c_str(), mqtt_username, mqtt_password)) {
+        //Serial.print("\tMTQQ Connected");
+        client.subscribe(subTopic);
+      }
+
+      //otherwise print failed for debugging
+      else { 
+        //Serial.println("\tFailed."); abort();
+      }
+    }
+  }
 }
 
 //generate unique name from MAC addr
-String macToStr(const uint8_t* mac){
-
-String result;
-
-for (int i = 0; i < 6; ++i) {
-result += String(mac[i], 16);
-
-if (i < 5){
-  result += ':';
-}
-}
-
-return result;
+String macToStr(const uint8_t* mac) {
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5) {
+      result += ':';
+    }
+  }
+  return result;
 }
 
